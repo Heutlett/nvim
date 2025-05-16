31b70ba3e6dc48a97490bc6ece24b3ba4cc9f2f2
aaraya <adrian.araya@ridgerun.com>
2025-05-16 16:07:35 -0600

    Add bidirectional copy paste

--- init.lua
+++ init.lua
@@ -12,6 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 	})
 end
 vim.opt.rtp:prepend(lazypath)
+vim.api.nvim_set_option("clipboard", "unnamedplus")
 
 require("vim-options")
 require("lazy").setup("plugins")
